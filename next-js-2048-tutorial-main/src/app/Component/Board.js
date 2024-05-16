a283fb09a84d8962c0168c968d8e1ae43ee31708
nekohasna <79271838+nekohasna@users.noreply.github.com>
2024-05-16 15:54:45 +0800

    Add files via upload

---
+++ next-js-2048-tutorial-main/src/app/Component/Board.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Tile from './Tile';
+
+const Board = ({ board }) => {
+  return (
+    <div className="board">
+        {board.flat().map((value, index) => (
+            <Tile key={index} value={value} />
+        ))}
+    </div>
+  );
+};
+
+export default Board;
